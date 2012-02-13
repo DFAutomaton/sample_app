ea6eaedad1380bd4912caaa4fc02e3a9ff40077c
Nathan M <dfautomaton@gmail.com>
2012-02-13 16:37:40 -0600

    end of chapter 7

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,12 +11,12 @@ describe UsersController do
    
   describe "GET 'new'" do
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
     end
 	
 	it "should have the right title" do
-	  get 'new'
+	  get :new
 	  response.should have_selector("title", :content => @base_title + "Sign Up")
 	end
   
