32aa40674e4a1ad317aa09f0981e838783d598a8
Nathan M <dfautomaton@gmail.com>
2012-02-13 15:36:03 -0600

    syncing with master

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+	user.name                  "Nathan Mall"
+	user.email                 "nathan@example.com"
+	user.password              "foobar"
+	user.password_confirmation "foobar"
+end
