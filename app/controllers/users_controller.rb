31b5fb3f1ce4d3e3b3991ef80c4d7a43207b8f59
Nathan M <dfautomaton@gmail.com>
2012-02-14 15:23:49 -0600

    finished 8.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
+
 	def new
 		@title="Sign Up"
+		@user = User.new #( :name => "foo bar", :email => "foo@bar.com", :password => "foobar", :password_confirmation => "foobar" )
 	end
   
 	def show
 		@user=User.find(params[:id])
 		@title=@user.name
 	end
+	
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			flash[:success] = "Welcome to the Sample App!"
+			redirect_to @user
+		else
+			@title="Sign Up"
+			render 'new'
+		end
+	end
+	
 end
