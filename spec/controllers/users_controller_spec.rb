c39b1c032000dbe0853752c1c270a9cbee9652a3
Nathan Mall <dfautomaton@gmail.com>
2012-02-14 22:17:16 -0600

    chapter 8 exercises

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -15,33 +15,53 @@ describe UsersController do
 			response.should be_success
 		end
 	
-	it "should have the right title" do
-		get :new
-		response.should have_selector("title", :content => "Sign Up")
-	end
+		it "should have the right title" do
+			get :new
+			response.should have_selector("title", :content => "Sign Up")
+		end
+		
+		it "should have a name field" do
+			get :new
+			response.should have_selector("input[name='user[name]'][type='text']")
+		end
+
+		it "should have an email field" do
+			get :new
+			response.should have_selector("input[name='user[email]'][type='text']")
+		end
+
+		it "should have a password field" do
+			get :new
+			response.should have_selector("input[name='user[password]'][type='password']")
+		end
+
+		it "should have a password confirmation field" do
+			get :new
+			response.should have_selector("input[name='user[password_confirmation]'][type='password']")
+		end
 	
 	end
 	
 	describe "GET 'show'" do
-	it "should have the right title" do
-		get :show, :id => @user
-		response.should have_selector("title", :content => @user.name)
-	end
-	
-	it "should find the right user" do
-		get :show, :id => @user
-		assigns(:user).should == @user
-	end
-	
-	it "should have the user's name" do
-		get :show, :id => @user
-		response.should have_selector("h1", :content => @user.name)
-	end
-	
-	it "should have a profile image" do
-		get :show, :id => @user
-		response.should have_selector("h1>img", :class => "gravatar")
-	end
+		it "should have the right title" do
+			get :show, :id => @user
+			response.should have_selector("title", :content => @user.name)
+		end
+		
+		it "should find the right user" do
+			get :show, :id => @user
+			assigns(:user).should == @user
+		end
+		
+		it "should have the user's name" do
+			get :show, :id => @user
+			response.should have_selector("h1", :content => @user.name)
+		end
+		
+		it "should have a profile image" do
+			get :show, :id => @user
+			response.should have_selector("h1>img", :class => "gravatar")
+		end
 	
 	end
 
